756318775c883768f4d83fce56b054f123fc179f
Patajuade <manon.diana@icloud.com>
2023-07-13 16:23:51 +0200

    update frontend, add post new character

--- Tests/frontend_test/src/app/Dashboard/page.tsx
+++ Tests/frontend_test/src/app/Dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import axios from "axios";
 import { useState, useEffect } from "react";
 
 interface Character {
@@ -8,65 +9,114 @@ interface Character {
     dateOfCreation: string;
     spec: string;
   }
-  
-  interface PageProps {
-    data: Character[];
-  }
-  
-//   const Page: React.FC<PageProps> = ({ data }) => {
-//     return (
-//       <div>
-//         {data.map((item) => (
-//           <div>{item.Name}</div>
-//         ))}
-//       </div>
-//     );
-//   };
-  
-//   export async function getServerSideProps() {
-//     const res = await fetch('https://localhost:7178/Character');
-//     const data: Character[] = await res.json();
-  
-//     return { props: { data } };
-//   }
-  
-//   export default Page;
 
-function Page() {
+  function Page() {
     const [data, setData] = useState<Character[] | null>(null);
+
+    const [name, setName] = useState('');
+    const [classType, setClassType] = useState('');
+    const [dateOfCreation, setDateOfCreation] = useState(new Date().toISOString());
+    const [spec, setSpec] = useState('');
+  
+    const loadData = async () => {
+      try {
+        const response = await axios.get('https://localhost:7178/Character');
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
   
     useEffect(() => {
-      fetch('https://localhost:7178/Character')
-        .then(response => response.json())
-        .then(json => setData(json))
-        .catch(error => console.error(error));
+      loadData();
     }, []);
   
+    const createCharacter = async (newCharacter: Character) => {
+      try {
+        await axios.post('https://localhost:7178/Character', newCharacter);
+        loadData(); // recharger les données après une mise à jour
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+    
+        const newCharacter = {
+          name: name,
+          class: classType,
+          dateOfCreation: dateOfCreation,
+          spec: spec,
+        };
+    
+        createCharacter(newCharacter);
+    
+        // Réinitialiser les champs du formulaire
+        setName('');
+        setClassType('');
+        setSpec('');
+      };
+  
+    const updateCharacter = async (updatedCharacter: Character) => {
+      try {
+        await axios.put(`https://localhost:7178/Character/${updatedCharacter.id}`, updatedCharacter);
+        loadData(); // recharger les données après une mise à jour
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    const deleteCharacter = async (id: string) => {
+      try {
+        await axios.delete(`https://localhost:7178/Character/${id}`);
+        loadData(); // recharger les données après une mise à jour
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
     return (
-        <div>
-            <table>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Class</th>
-                  <th>Spec</th>
-                  <th>Date of Creation</th>
+      <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Class</th>
+                <th>Spec</th>
+                <th>Date of Creation</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.class}</td>
+                  <td>{item.spec}</td>
+                  <td>{item.dateOfCreation}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {data?.map((item, index) => (
-                  <tr key={index}>
-                    <td>{item.name}</td>
-                    <td>{item.class}</td>
-                    <td>{item.spec}</td>
-                    <td>{item.dateOfCreation}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-        </div>
-      );
-    }
+              ))}
+            </tbody>
+          </table>
+
+          <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} required />
+        </label>
+        <label>
+          Class:
+          <input type="text" value={classType} onChange={e => setClassType(e.target.value)} required />
+        </label>
+        <label>
+          Spec:
+          <input type="text" value={spec} onChange={e => setSpec(e.target.value)} required />
+        </label>
+        <input type="submit" value="Create" />
+      </form>
+      </div>
+    );
+  }
   
   export default Page;
   
