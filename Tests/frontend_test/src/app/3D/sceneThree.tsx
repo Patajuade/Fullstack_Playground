77fb853a14801aa508d41453188c176d45fa74b0
Patajuade <manon.diana@icloud.com>
2023-09-20 16:26:50 +0200

    update 3D

--- Tests/frontend_test/src/app/3D/sceneThree.tsx
+++ Tests/frontend_test/src/app/3D/sceneThree.tsx
@@ -1,29 +1,49 @@
 "use client";
-
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Suspense, useRef, useState } from "react";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+
+export const Light = () => {
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={1} />
+    </>
+  );
+};
 
 export const Human3D = () => {
   const obj = useLoader(OBJLoader, "/assets/3D/human_base_meshes_bundle.obj");
-  const directionalLightRef = useRef();
-  const backlightref = useRef();
-  const cameraRef = useRef();
+  const [isDragging, setIsDragging] = useState(false);
+  const groupRef = useRef();
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+
+    groupRef.current.rotation.y += e.movementX / 100;
+  };
 
   return (
-    <group>
-      <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 2.5, 10]} />
-      <OrbitControls enableRotate={true} enableZoom={true} enablePan={true} />
-      <ambientLight intensity={0.2} />
+    <group
+      ref={groupRef}
+      onPointerDown={handleMouseDown}
+      onPointerUp={handleMouseUp}
+      onPointerMove={handleMouseMove}
+      onClick={(event) => {
+        console.log(event);
+      }}
+    >
       <mesh position={[0, -2.5, 0]}>
-        <primitive
-          object={obj}
-          scale={3}
-          onClick={(event: Event) => {
-            console.log(event);
-          }}
-        />
+        <primitive object={obj} scale={3} />
       </mesh>
     </group>
   );
@@ -32,15 +52,8 @@ export const Human3D = () => {
 export const SceneThree = () => {
   return (
     <Canvas>
-      <Suspense>
-        <directionalLight position={[0, 2.5, 10]} />
-        <directionalLight
-          intensity={1.5}
-          position={[0, 2.5, -300]}
-          rotateY={180}
-        />
-        <Human3D />
-      </Suspense>
+      <Light />
+      <Human3D />
     </Canvas>
   );
 };
