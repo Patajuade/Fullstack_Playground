f9a652b7a75cacad31ac9d841914601e9d3784c7
Patajuade <manon.diana@icloud.com>
2023-09-21 11:09:10 +0200

    add working light, add click and drag, add first version of zoom on model

--- Tests/frontend_test/src/app/3D/sceneThree.tsx
+++ Tests/frontend_test/src/app/3D/sceneThree.tsx
@@ -1,46 +1,67 @@
 "use client";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 export const Light = () => {
   return (
     <>
-      <ambientLight intensity={0.5} />
-      <directionalLight intensity={1} />
+      <directionalLight intensity={1} position={[0, 0, 10]} />
     </>
   );
 };
 
 export const Human3D = () => {
   const obj = useLoader(OBJLoader, "/assets/3D/human_base_meshes_bundle.obj");
-  const [isDragging, setIsDragging] = useState(false);
   const groupRef = useRef();
+  const [isDragging, setIsDragging] = useState(false);
+  const previousMousePosition = useRef([0, 0]);
 
-  const handleMouseDown = () => {
+  const handleMouseClickAndDrag = (e) => {
     setIsDragging(true);
-  };
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
+    previousMousePosition.current = [e.clientX, e.clientY];
   };
 
   const handleMouseMove = (e) => {
-    if (!isDragging) return;
+    if (isDragging) {
+      const [deltaX, deltaY] = [
+        e.clientX - previousMousePosition.current[0],
+        e.clientY - previousMousePosition.current[1],
+      ];
 
-    groupRef.current.rotation.y += e.movementX / 100;
+      groupRef.current.rotation.y += deltaX / 100;
+      groupRef.current.rotation.x += deltaY / 100;
+
+      previousMousePosition.current = [e.clientX, e.clientY];
+    }
   };
+
+  const handleMouseWheel = (e) => {
+    const zoomSpeed = 0.001; // Ajustez la vitesse de zoom selon vos besoins
+    groupRef.current.scale.x += e.deltaY * zoomSpeed;
+    groupRef.current.scale.y += e.deltaY * zoomSpeed;
+    groupRef.current.scale.z += e.deltaY * zoomSpeed;
+  };
+
+  useEffect(() => {
+    const handleDocumentMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    document.addEventListener("mouseup", handleDocumentMouseUp);
+
+    return () => {
+      document.removeEventListener("mouseup", handleDocumentMouseUp);
+    };
+  }, []);
 
   return (
     <group
       ref={groupRef}
-      onPointerDown={handleMouseDown}
-      onPointerUp={handleMouseUp}
+      onPointerDown={handleMouseClickAndDrag}
       onPointerMove={handleMouseMove}
-      onClick={(event) => {
-        console.log(event);
-      }}
+      onWheel={handleMouseWheel}
     >
       <mesh position={[0, -2.5, 0]}>
         <primitive object={obj} scale={3} />
