3bdc274a9f3c868c7546565936e94c7ae0dcdf31
Patajuade <manon.diana@icloud.com>
2023-09-28 09:33:18 +0200

    update cleanup

--- Tests/frontend_test/src/app/3D/sceneThree.tsx
+++ Tests/frontend_test/src/app/3D/sceneThree.tsx
@@ -6,78 +6,17 @@ import {
   useLoader,
   useThree,
 } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { log } from "console";
 
 extend({ OrbitControls });
-
-export const Light = ({ lightRef }) => {
-  return <></>;
-};
 
 export const Human3D = () => {
   const obj = useLoader(OBJLoader, "/assets/3D/human_base_meshes_bundle.obj");
   const groupRef = useRef<GroupProps>();
-  const [isDragging, setIsDragging] = useState(false);
-  const previousMousePosition = useRef([0, 0]);
-
-  const handleMouseClickAndDrag = (e) => {
-    setIsDragging(true);
-    previousMousePosition.current = [e.clientX, e.clientY];
-  };
-
-  // const handleMouseMove = (e) => {
-  //   if (isDragging) {
-  //     const [deltaX, deltaY] = [
-  //       e.clientX - previousMousePosition.current[0],
-  //       e.clientY - previousMousePosition.current[1],
-  //     ];
-
-  //     groupRef.current.rotation.y += deltaX / 100;
-  //     groupRef.current.rotation.x += deltaY / 100;
-
-  //     previousMousePosition.current = [e.clientX, e.clientY];
-  //   }
-  // };
-
-  const handleMouseWheel = (e) => {
-    if (!groupRef.current) return;
-
-    const zoomSpeed = 0.001;
-    // Obtenir la position du pointeur par rapport au modèle
-    const pointerPosition = e.point.clone();
-
-    // Effectuer le zoom en fonction de la position du pointeur
-    const zoomFactor = 1 + e.deltaY * zoomSpeed;
-    groupRef.current.scale.multiplyScalar(zoomFactor);
-
-    // Ajuster la position du modèle pour maintenir le pointeur au même endroit
-    const newModelPosition = pointerPosition
-      .clone()
-      .multiplyScalar(1 - zoomFactor);
-    groupRef.current.position.add(newModelPosition);
-  };
-
-  useEffect(() => {
-    const handleDocumentMouseUp = () => {
-      setIsDragging(false);
-    };
-
-    document.addEventListener("mouseup", handleDocumentMouseUp);
-
-    return () => {
-      document.removeEventListener("mouseup", handleDocumentMouseUp);
-    };
-  }, []);
-
   return (
-    <group
-      ref={groupRef}
-      onPointerDown={handleMouseClickAndDrag}
-      onWheel={handleMouseWheel}
-    >
+    <group ref={groupRef}>
       <mesh position={[0, -2.5, 0]}>
         <primitive object={obj} scale={3} />
       </mesh>
@@ -111,15 +50,3 @@ export const SceneThree = () => {
     </>
   );
 };
-
-// plusieurs façons de faire :
-// 1
-// On peut utiliser orbitControls pour gérer le zoom et la rotation
-// Avantage : zoom et rotation fonctionnent très bien
-// Désavantage : la lumière reste collée à une face cu modèle
-// Solution : éclairer tout le modèle
-// 2
-// On peut utiliser les fonctions de rotation et de zoom de la souris
-// Avantage : la lumière éclaire comme il faut, a le comportement d'un "soleil"
-// Désavantage : la rotation est un peu saccadée, ne fonctionne plus quand on sort du mesh, le modèle se fait la malle avec le zoom
-// Solution : mieux faire la rotation, le zoom,...
