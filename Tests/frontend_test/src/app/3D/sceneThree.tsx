07af0988529b21c86674c8d436c8030fdcb04931
Patajuade <manon.diana@icloud.com>
2023-09-29 16:37:07 +0200

    add electedbodyparts to array in human3D

--- Tests/frontend_test/src/app/3D/sceneThree.tsx
+++ Tests/frontend_test/src/app/3D/sceneThree.tsx
@@ -22,17 +22,35 @@ interface SceneThreeProps {
 export const Human3D = ({ setBodyPart }: Human3DProps) => {
   const obj = useLoader(OBJLoader, "/assets/3D/human_base_meshes_bundle.obj");
   const groupRef = useRef<GroupProps>();
+  const [selectedBodyParts, setSelectedBodyParts] = useState<string[]>([]);
 
   const handleClick = (event: any) => {
     setBodyPart(event.intersections[0].object.name);
     const intersections: Intersection[] = event.intersections;
     const element = intersections[0].object;
+    const clickedPart = event.intersections[0].object.name;
+
+    // Si la partie du corps est déjà sélectionnée, on la déselectionne
+    if (selectedBodyParts.includes(clickedPart)) {
+      setSelectedBodyParts((prevSelectedParts) =>
+        prevSelectedParts.filter((part) => part !== clickedPart)
+      );
+      setBodyPart("");
+      // Sinon, on ajoute la partie du corps au tableau des parties sélectionnées
+    } else {
+      setSelectedBodyParts((prevSelectedParts) => [
+        ...prevSelectedParts,
+        clickedPart,
+      ]);
+      setBodyPart(clickedPart);
+    }
+
     if (element.material.color.getHexString() === "ff0000") {
       element.material.color.set("#ffffff");
-      setBodyPart("");
       return;
     }
     element.material.color.set("#ff0000");
+    console.log(selectedBodyParts);
   };
 
   return (
@@ -65,7 +83,7 @@ export const SceneThree = ({ setBodyPart }: SceneThreeProps) => {
   const humanRef = useRef();
   const lightRef = useRef();
   const orbitControlsRef = useRef();
-  const { camera, gl } = useThree();
+  const { camera } = useThree();
 
   const resetCamera = (event: KeyboardEvent) => {
     if (event.key === "r") {
@@ -87,7 +105,12 @@ export const SceneThree = ({ setBodyPart }: SceneThreeProps) => {
 
   return (
     <>
-      <OrbitControls makeDefault ref={orbitControlsRef} />
+      <OrbitControls
+        makeDefault
+        ref={orbitControlsRef}
+        enableDamping={false}
+        enablePan={false}
+      />
       <directionalLight ref={lightRef} intensity={1} position={[0, 0, 10]} />
       <ambientLight intensity={0.1} />
       <mesh ref={humanRef}>
